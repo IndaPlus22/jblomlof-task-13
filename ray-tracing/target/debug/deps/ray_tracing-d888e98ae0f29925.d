5e9dadfaa876bf64872de8fe8a9f07c6605bbada
jonathan-blomlof <joning.b@hotmail.com>
2023-01-17 22:24:40 +0100

    Clean up.

--- ray-tracing/target/debug/deps/ray_tracing-d888e98ae0f29925.d
+++
@@ -1,7 +0,0 @@
-c:\rustprogramming\ray-tracing\jblomlof-task-13\ray-tracing\target\debug\deps\ray_tracing-d888e98ae0f29925.rmeta: src\main.rs src\ray.rs src\world.rs
-
-c:\rustprogramming\ray-tracing\jblomlof-task-13\ray-tracing\target\debug\deps\ray_tracing-d888e98ae0f29925.d: src\main.rs src\ray.rs src\world.rs
-
-src\main.rs:
-src\ray.rs:
-src\world.rs:
--- ray-tracing/target/debug/deps/ray_tracing.d
+++
@@ -1,7 +0,0 @@
-C:\rustprogramming\ray-tracing\jblomlof-task-13\ray-tracing\target\debug\deps\ray_tracing.exe: src\main.rs src\ray.rs src\world.rs
-
-C:\rustprogramming\ray-tracing\jblomlof-task-13\ray-tracing\target\debug\deps\ray_tracing.d: src\main.rs src\ray.rs src\world.rs
-
-src\main.rs:
-src\ray.rs:
-src\world.rs:
